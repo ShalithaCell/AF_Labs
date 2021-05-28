6b4639860fd99b7b50993b8f6f862b8cec57c062
shalitha <shalithax@gmail.com>
2021-05-29 00:24:49 +0530

    lab 5 complete

---
+++ Lab5/routes/home.router.js
@@ -0,0 +1,16 @@
+// require “koa” dependency
+const Router = require('koa-router');
+
+const router = new Router({
+    prefix: '/home'
+});
+router.get('/', ctx => {
+    ctx.body = 'Hello World';
+});
+router.post('/', ctx => {
+    ctx.body = 'Hello World';
+});
+
+module.exports = {
+    HomeRoutes : router,
+}
